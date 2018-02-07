2cd38b0e91bc15e3be5f339bac31ba6ba838619c
POPEYE\diego <diegomazala@gmail.com>
2018-02-07 14:17:12 -0200

    Stypegrip console C# program working

--- csharp/Tracking/StypeGrip.cs
+++ csharp/Tracking/StypeGrip.cs
@@ -142,6 +142,17 @@ namespace Tracking
                 }
             }
 
+            public float[] XYZ
+            {
+                get
+                {
+                    return new float[]{
+                        System.BitConverter.ToSingle(data, DataIndex.X),
+                        System.BitConverter.ToSingle(data, DataIndex.Y),
+                        System.BitConverter.ToSingle(data, DataIndex.Z)};
+                }
+            }
+
 
             // Rotation (pan, tilt, roll) in euler angles
             public UnityEngine.Vector3 EulerAngles
@@ -152,6 +163,17 @@ namespace Tracking
                         System.BitConverter.ToSingle(data, DataIndex.Tilt),
                         System.BitConverter.ToSingle(data, DataIndex.Pan),
                         System.BitConverter.ToSingle(data, DataIndex.Roll));
+                }
+            }
+
+            public float[] PanTiltRoll
+            {
+                get
+                {
+                    return new float[]{
+                        System.BitConverter.ToSingle(data, DataIndex.Pan),
+                        System.BitConverter.ToSingle(data, DataIndex.Tilt),
+                        System.BitConverter.ToSingle(data, DataIndex.Roll)};
                 }
             }
 
@@ -264,6 +286,15 @@ namespace Tracking
 
                     return (check == Checksum);
                 }
+            }
+
+            public override string ToString()
+            {
+                return 
+                    this.Counter + ' ' + this.Timecode + '\n' +
+                    this.Position.ToString() + '\n' + this.Rotation.ToString() + '\n' +
+                    this.Zoom + ' ' + this.Focus + '\n' +
+                    this.K1 + ' ' + this.K2;
             }
         };
 
@@ -496,6 +527,144 @@ namespace Tracking
         };
 
 
+        [System.Serializable]
+        public class NetReader<T> : Tracking.NetReader<T>
+        {
+            public Xcito.CameraConstants CameraConsts = new Xcito.CameraConstants();
+
+
+            protected override void ReadDataThread()
+            {
+                IPEndPoint receivedEP = remoteEP;
+
+                bool consume_while_available = Config.ConsumeWhileAvailable;
+                int read_interval_ms = Config.ReadIntervalMs;
+
+                lock (threadLocked)
+                {
+                    threadRunning = true;
+                    TotalCounter = 0;
+                }
+
+                //
+                // Receive the very first packages in order to reset the counters
+                // 
+                byte[] received_data = client.Receive(ref receivedEP);
+
+
+
+                if (received_data.Length == PacketHF.DataIndex.Total 
+                    || received_data.Length == PacketA5.DataIndex.Total)    //  Is a valid packet
+                {
+                    Buffer.Insert((T)System.Activator.CreateInstance(typeof(T), received_data));
+                }
+                else
+                {
+                    System.Console.WriteLine("Unknown Format/Encoding");
+                }
+
+
+                Buffer.ResetDrops();
+
+                while (threadRunning)
+                {
+                    Thread.Sleep(read_interval_ms);
+
+                    if (client.Available < 1)
+                        continue;
+
+                    received_data = client.Receive(ref receivedEP);
+
+                    while (consume_while_available && client.Available > 0)
+                    {
+                        received_data = client.Receive(ref receivedEP);
+                    }
+
+                    bool isRightHost = (remoteEP.Address.Equals(receivedEP.Address)) || remoteEP.Address.Equals(IPAddress.Any);
+                    bool isRightPort = (remoteEP.Port == receivedEP.Port) || remoteEP.Port == 0;
+
+                    if (!isRightHost || !isRightPort)
+                        continue;
+
+                    lock (threadLocked)
+                    {
+                        Buffer.Insert((T)System.Activator.CreateInstance(typeof(T), received_data));
+                        TotalCounter++;
+
+                        //System.Console.WriteLine(" ===========================> {0}", received_data.Length);
+                        //string[] words = ASCIIEncoding.ASCII.GetString(received_data, 0, received_data.Length).Split(' ');
+                        ////string[] words = ASCIIEncoding.ASCII.GetString(received_data, 9, received_data.Length - 9).Split(' ');
+                        //for (int i = 0; i < words.Length; ++i)
+                        //{
+                        //    System.Console.WriteLine("{0} - {1}", i, words[i]);
+                        //}
+                    }
+
+
+                }
+            }
+        }
+
+
+        [System.Serializable]
+        public class NetReaderAsync<T> : Tracking.NetReaderAsync<T>
+        {
+            public override void Connect(Tracking.Config config, Tracking.IRingBuffer<T> ringBuffer)
+            {
+                base.Connect(config, ringBuffer);
+
+                // Wait a bit to reset the number of drops in ring buffer
+                Thread.Sleep(50);
+                Buffer.ResetDrops();
+            }
+
+
+            protected override void DataReceived(System.IAsyncResult ar)
+            {
+                UdpState state = (UdpState)ar.AsyncState;
+
+                try
+                {
+                    IPEndPoint wantedIpEndPoint = (IPEndPoint)state.RemoteEndPoint;
+                    IPEndPoint receivedIpEndPoint = (IPEndPoint)state.LocalEndPoint; ;
+
+                    System.Byte[] data = state.Client.EndReceive(ar, ref receivedIpEndPoint);
+
+                    // Check sender
+                    bool isRightHost = (wantedIpEndPoint.Address.Equals(receivedIpEndPoint.Address)) || wantedIpEndPoint.Address.Equals(IPAddress.Any);
+                    bool isRightPort = (wantedIpEndPoint.Port == receivedIpEndPoint.Port) || wantedIpEndPoint.Port == 0;
+                    if (isRightHost && isRightPort)
+                    {
+                        if (data[6] == 'P' && data[7] == 'A')  // Parameters && ASCII
+                        {
+                            Buffer.Insert((T)System.Activator.CreateInstance(typeof(T), data));
+                        }
+                        else if (data[6] == 'C' && data[7] == 'A')  // Constants && ASCII
+                        {
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Unknown Format/Encoding");
+                        }
+
+                        TotalCounter++;
+                    }
+
+                    // Restart listening for udp data packages
+                    state.Client.BeginReceive(new System.AsyncCallback(DataReceived), ar.AsyncState);
+                }
+                catch (System.ObjectDisposedException)
+                {
+                    // No problem. Socket has been closed.
+                    //System.Console.WriteLine(e.ToString());
+                }
+                catch (System.Exception e)
+                {
+                    System.Console.WriteLine(e.ToString());
+                }
+            }
+
+        }
     }   // end StypeGrip namespace
 
 }   // end Tracking namespace
---
+++ csharp/Tracking/TrackingStype.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+
+
+public class TrackingApp
+{
+    public static void PrintData(Tracking.StypeGrip.PacketHF packet)
+    {
+        System.Console.WriteLine("---------------------------------------------------");
+        System.Console.WriteLine("Counter: {0}", packet.Counter);
+        System.Console.WriteLine("Time   : {0}", packet.Timecode);
+        System.Console.WriteLine("Pos    : {0}, {1}, {2}", packet.XYZ[0], packet.XYZ[1], packet.XYZ[2]);
+        System.Console.WriteLine("Rot    : {0}, {1}, {2}", packet.PanTiltRoll[0], packet.PanTiltRoll[1], packet.PanTiltRoll[2]);
+        System.Console.WriteLine("Fov    : {0} {1}", packet.FovX, packet.FovY);
+        System.Console.WriteLine("Zoom   : {0}", packet.Zoom);
+        System.Console.WriteLine("Focus  : {0}", packet.Focus);
+        System.Console.WriteLine("K1/K2  : {0} {1}", packet.K1, packet.K2);
+        System.Console.WriteLine("\n---------------------------------------------------");
+    }
+
+
+    public static void PrintStats(Tracking.RingBuffer<Tracking.StypeGrip.PacketHF> ringBuffer)
+    {
+        System.Console.WriteLine("---------------------------------------------------");
+        System.Console.WriteLine("Time   : {0}", DateTime.Now);
+        System.Console.WriteLine("Drops  : {0}", ringBuffer.Drops);
+        System.Console.Write("Buffer :");
+        for (int i = 0; i < ringBuffer.Length; ++i)
+            System.Console.Write(" {0}", ringBuffer.Data[i].Counter);
+        System.Console.WriteLine("\n---------------------------------------------------");
+    }
+
+    public static void PrintConfig(Tracking.StypeGrip.Config config)
+    {
+        System.Console.WriteLine("\n---------------------------------------------------");
+        System.Console.WriteLine("Local Ip              : {0}", config.LocalIp);
+        System.Console.WriteLine("Remote Ip             : {0}", config.RemoteIp);
+        System.Console.WriteLine("Port                  : {0}", config.Port);
+        System.Console.WriteLine("Delay                 : {0}", config.Delay);
+        System.Console.WriteLine("ReadIntervalMs        : {0}", config.ReadIntervalMs);
+        System.Console.WriteLine("ConsumeWhileAvailable : {0}", config.ConsumeWhileAvailable);
+        System.Console.WriteLine("---------------------------------------------------");
+    }
+
+    public static void PrintHelp()
+    {
+        System.Console.WriteLine("\n---------------------------------------------------");
+        System.Console.WriteLine("[Enter]   - Show stats");
+        System.Console.WriteLine("[Space]   - Start/Stop stats loop");
+        System.Console.WriteLine("[C]       - Clear");
+        System.Console.WriteLine("[N]       - Network config");
+        System.Console.WriteLine("[H]       - Help");
+        System.Console.WriteLine("[Esc]     - Exit");
+        System.Console.WriteLine("---------------------------------------------------\n");
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Usage: TrackingApp.exe Host Port Delay ReadInterval ConsumeWhileAvailable");
+        Tracking.StypeGrip.Config config = new Tracking.StypeGrip.Config();
+        config.LocalIp = "127.0.0.1";
+        config.RemoteIp = "0.0.0.0";
+        config.Port = 12000;
+        config.Delay = 5;
+        config.ReadIntervalMs = 10;
+        config.ConsumeWhileAvailable = false;
+
+        if (args.Length > 1)
+        {
+            config.LocalIp = args[0];
+            config.RemoteIp = args[1];
+            System.Int32.TryParse(args[2], out config.Port);
+            System.Int32.TryParse(args[3], out config.Delay);
+            System.Int32.TryParse(args[4], out config.ReadIntervalMs);
+            System.Boolean.TryParse(args[5], out config.ConsumeWhileAvailable);
+        }
+        else
+        {
+            System.Console.WriteLine("Running Default Configuration");
+        }
+
+        PrintConfig(config);
+
+
+        Tracking.INetReader<Tracking.StypeGrip.PacketHF> netReader
+            //= new Tracking.StypeGrip.NetReaderAsync<Tracking.StypeGrip.PacketHF>();
+            = new Tracking.StypeGrip.NetReader<Tracking.StypeGrip.PacketHF>();
+        Tracking.RingBuffer<Tracking.StypeGrip.PacketHF> ringBuffer = new Tracking.RingBuffer<Tracking.StypeGrip.PacketHF>(config.Delay);
+
+        netReader.Config = config;
+        netReader.Buffer = ringBuffer;
+
+        netReader.Connect(config, ringBuffer);
+        ringBuffer.ResetDrops();
+
+
+        PrintHelp();
+
+
+        bool exit = false;
+        bool continuous_print = false;
+#if false
+        do
+        {
+            uint lastDropCounter = ringBuffer.Drops;
+            while (!System.Console.KeyAvailable)
+            {
+                if (netReader.IsReading)
+                {
+                    for (int i = 0; i < ringBuffer.Length; ++i)
+                        System.Console.Write(" {0}", ringBuffer.Data[i].Counter);
+                    System.Console.WriteLine(" --> {0}", ringBuffer.Drops);
+
+                    if (lastDropCounter != ringBuffer.Drops)
+                    {
+                        lastDropCounter = ringBuffer.Drops;
+                        System.Console.WriteLine("-------------------->> {0} : {1}", lastDropCounter, System.DateTime.Now);
+                    }
+
+                    Thread.Sleep(33);
+                }
+            }
+
+        } while (System.Console.ReadKey(true).Key != System.ConsoleKey.Escape);
+#else
+        while(!exit)
+        {
+            if (System.Console.KeyAvailable)
+            {
+                System.ConsoleKey key_pressed = System.Console.ReadKey(true).Key;
+                if (key_pressed == System.ConsoleKey.Spacebar)
+                {
+                    continuous_print = !continuous_print;
+                }
+                else if (key_pressed == System.ConsoleKey.Enter)
+                {
+                    PrintData(ringBuffer.Packet);
+                    PrintStats(ringBuffer);
+                }
+                else if (key_pressed == System.ConsoleKey.C)
+                {
+                    System.Console.Clear();
+                }
+                else if (key_pressed == System.ConsoleKey.N)
+                {
+                    PrintConfig(config);
+                }
+                else if (key_pressed == System.ConsoleKey.H)
+                {
+                    PrintHelp();
+                }
+                else if (key_pressed == System.ConsoleKey.Escape)
+                {
+                    PrintStats(ringBuffer);
+                    exit = true;
+                }
+            }
+            else if (continuous_print)
+            {
+                PrintStats(ringBuffer);
+            }
+            
+        }
+#endif
+
+        netReader.Disconnect();
+    }
+}
