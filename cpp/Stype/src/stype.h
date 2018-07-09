1d776da61bafa73205b0257ea430eaa32bd1948a
AVELL_TVG\diego <diegomazala@gmail.com>
2018-07-09 19:09:25 -0300

    Initial code for stype in c++

---
+++ cpp/Stype/src/stype.h
@@ -0,0 +1,179 @@
+#ifndef __STYPE_PACKET_HF__
+#define __STYPE_PACKET_HF__
+
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+class Property 
+{
+public:
+	virtual ~Property() {}
+	virtual T & operator = (const T &f) { return value = f; }
+	virtual operator T const & () const { return value; }
+protected:
+	T value;
+};
+
+namespace stype
+{
+	struct buffer_index 
+	{
+		static const uint8_t header			= 0;
+		static const uint8_t command		= 1;
+		static const uint8_t timecode		= 2;
+		static const uint8_t packagenumber	= 5;
+		static const uint8_t x				= 6;
+		static const uint8_t y				= 10;
+		static const uint8_t z				= 14;
+		static const uint8_t pan			= 18;
+		static const uint8_t tilt			= 22;
+		static const uint8_t roll			= 26;
+		static const uint8_t fovx			= 30;
+		static const uint8_t aspectratio	= 34;
+		static const uint8_t focus			= 38;
+		static const uint8_t zoom			= 42;
+		static const uint8_t k1				= 46;
+		static const uint8_t k2				= 50;
+		static const uint8_t centerx		= 54;
+		static const uint8_t centery		= 58;
+		static const uint8_t chipwidth		= 62;
+		static const uint8_t checksum		= 66;
+		static const uint8_t total			= 67;
+	};
+
+	struct packet_index
+	{
+		static const uint8_t header = 0;
+		static const uint8_t command = 1;
+		static const uint8_t timecode_0 = 2;
+		static const uint8_t timecode_1 = 3;
+		static const uint8_t timecode_2 = 4;
+		static const uint8_t packagenumber = 5;
+		static const uint8_t x = 0;
+		static const uint8_t y = 1;
+		static const uint8_t z = 2;
+		static const uint8_t pan = 3;
+		static const uint8_t tilt = 4;
+		static const uint8_t roll = 5;
+		static const uint8_t fovx = 6;
+		static const uint8_t aspectratio = 7;
+		static const uint8_t focus = 8;
+		static const uint8_t zoom = 9;
+		static const uint8_t k1 = 10;
+		static const uint8_t k2 = 11;
+		static const uint8_t centerx = 12;
+		static const uint8_t centery = 13;
+		static const uint8_t chipwidth = 14;
+	};
+
+	class packet
+	{
+	public:
+
+		packet(uint8_t* buffer)
+		{
+			from_buffer(buffer);
+			
+		}
+
+		void from_buffer(uint8_t* buffer)
+		{
+			std::copy(
+				(&buffer[buffer_index::header]),
+				(&buffer[buffer_index::header]) + info.size(),
+				info.begin());
+
+			std::copy(
+				reinterpret_cast<float*>(&buffer[buffer_index::x]), 
+				reinterpret_cast<float*>(&buffer[buffer_index::x]) + params.size(), 
+				params.begin());
+
+			is_valid = checksum(buffer);
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const packet& p)
+		{
+			os << unsigned(p.info[0]) << ' ' << unsigned(p.info[1]) << ' ' << p.timecode() << ' ' << unsigned(p.info[5]) << ' ';
+			for (const auto i : p.params)
+				os << i << ' ';
+			return os;
+		}
+
+
+		bool checksum(uint8_t* buffer) const
+		{
+			uint8_t check = 0;
+			for (uint8_t x = 0; x < buffer_index::checksum; ++x)
+				check += buffer[x];
+			return check == buffer[buffer_index::checksum];
+		}
+		
+
+		uint8_t header() const { return info[packet_index::header]; }
+		void header(uint8_t val) { info[packet_index::header] = val; }
+
+		uint8_t command() const { return info[packet_index::command]; }
+		void command(uint8_t val) {info[packet_index::command] = val; }
+
+		uint8_t package_number() const { return info[packet_index::packagenumber]; }
+		void package_number(uint8_t val) { info[packet_index::packagenumber] = val; }
+		
+		uint32_t timecode() const { return (info[packet_index::timecode_2] << 16) | (info[packet_index::timecode_1] << 8) | (info[packet_index::timecode_0] << 0);	}
+		void timecode(uint32_t val)
+		{
+			info[packet_index::timecode_0] = ((uint8_t*)&val)[0];
+			info[packet_index::timecode_1] = ((uint8_t*)&val)[1];
+			info[packet_index::timecode_2] = ((uint8_t*)&val)[2];
+		}
+
+		float x() const { return params[packet_index::x]; }
+		void x(float val) { params[packet_index::x] = val; }
+
+		float y() const { return params[packet_index::y]; }
+		void y(float val) { params[packet_index::y] = val; }
+
+		float z() const { return params[packet_index::z]; }
+		void z(float val) { params[packet_index::z] = val; }
+
+		float pan() const { return params[packet_index::pan]; }
+		void pan(float val) { params[packet_index::pan] = val; }
+
+		float tilt() const { return params[packet_index::tilt]; }
+		void til(float val) { params[packet_index::tilt] = val; }
+
+		float roll() const { return params[packet_index::roll]; }
+		void roll(float val) { params[packet_index::roll] = val; }
+
+		float fovx() const { return params[packet_index::fovx]; }
+		void fovx(float val) { params[packet_index::fovx] = val; }
+
+		float aspect_ratio() const { return params[packet_index::aspectratio]; }
+		void aspect_ratio(float val) { params[packet_index::aspectratio] = val; }
+
+		float k1() const { return params[packet_index::k1]; }
+		void k1(float val) { params[packet_index::k1] = val; }
+
+		float k2() const { return params[packet_index::k2]; }
+		void k2(float val) { params[packet_index::k2] = val; }
+
+		float center_x() const { return params[packet_index::centerx]; }
+		void center_x(float val) { params[packet_index::centerx] = val; }
+
+		float center_y() const { return params[packet_index::centery]; }
+		void center_y(float val) { params[packet_index::centery] = val; }
+
+		float chip_width() const { return params[packet_index::chipwidth]; }
+		void chip_width(float val) { params[packet_index::chipwidth] = val; }
+
+	private:
+		std::vector<uint8_t>	info	= std::vector<uint8_t>((buffer_index::x - buffer_index::header) / sizeof(uint8_t));	// 6 elems
+		std::vector<float>		params	= std::vector<float>((buffer_index::checksum - buffer_index::x) / sizeof(float));	// 15 elems
+		bool					is_valid = false;	// holds a flag to indicate if this packet was checksumed successfully
+	};
+
+	
+}
+
+#endif // __STYPE_PACKET_HF__
---
+++ cpp/Stype/src/stype_net_read.cpp
@@ -0,0 +1,82 @@
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/array.hpp>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <conio.h> 
+
+#include "stype.h"	
+
+class udp_client
+{
+public:
+	
+	udp_client(boost::asio::io_service& io_service, uint16_t port):
+		socket(io_service, { boost::asio::ip::udp::v4(), port })
+	{
+	}
+
+
+	void start_receive()
+	{
+		socket.async_receive_from(boost::asio::buffer(recv_buffer), receiver_endpoint,
+			boost::bind(&udp_client::handle_receive, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+	}
+
+	void handle_receive(const boost::system::error_code& error, size_t bytes_transferred)
+	{
+		//std::cout << "\n[" << bytes_transferred << " bytes] received : ";
+
+		stype::packet packet(recv_buffer.data());
+
+		std::cout
+		//	<< std::fixed << packet.timecode()
+			<< ' ' << packet << std::endl;
+
+
+		if (!error || error == boost::asio::error::message_size)
+			start_receive();
+	}
+private:
+	boost::asio::ip::udp::socket socket;
+	boost::asio::ip::udp::endpoint receiver_endpoint;
+	boost::array<uint8_t, 256> recv_buffer;
+
+};
+
+
+static boost::asio::io_service io_service;
+static std::mutex mutex;
+
+void run_udp_client(uint16_t port)
+{
+	udp_client updclient(io_service, port);
+	updclient.start_receive();
+
+	io_service.run();
+}
+
+
+int main(int argc, char* argv[])
+{
+	uint16_t port = 10002;
+	if (argc > 1)
+		port = std::atoi(argv[1]);
+
+	std::thread udp_receive_thread(run_udp_client, port);
+	
+	while (getch() != 27) {}	// wait for escape key
+
+	mutex.lock();
+	{
+		io_service.stop();
+	}
+	mutex.unlock();
+	
+	udp_receive_thread.join();
+
+	return EXIT_SUCCESS;
+}
